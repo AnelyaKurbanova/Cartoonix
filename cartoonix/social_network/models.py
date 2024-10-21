4c71249ae6f48696317f563bfd2ee3938a1cc7c5
k4sper1love <elkin_serega.2004@mail.ru>
2024-10-21 20:26:25 +0500

    feat: add video url field

--- cartoonix/social_network/models.py
+++ cartoonix/social_network/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django.db import models
 from PIL import Image
 from django.utils import timezone
+from ai.models import VideoPrompt
 
 
 class Post(models.Model):
@@ -10,6 +11,12 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default_post.jpg', upload_to='post_pics')
+    video_url = models.OneToOneField(
+        VideoPrompt,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+    )
 
     def __str__(self):
         return self.title
--- cartoonix/social_network/serializers.py
+++ cartoonix/social_network/serializers.py
@@ -2,13 +2,50 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import Post, Comment, Like, Profile
 
+from rest_framework import serializers
+from .models import Post
+from ai.models import VideoPrompt
+
+class VideoPromptSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = VideoPrompt
+        fields = ['id', 'prompt_text']  # Укажите необходимые поля
+
 class PostSerializer(serializers.ModelSerializer):
-    total_likes = serializers.ReadOnlyField()
+    video_url = VideoPromptSerializer(required=False)  # Укажите, что поле опциональное
 
     class Meta:
         model = Post
-        fields = ['id', 'title', 'content', 'created_at', 'author', 'total_likes', 'image']
-        read_only_fields = ['author', 'created_at']
+        fields = ['id', 'title', 'content', 'created_at', 'author', 'image', 'video_url']
+
+    def create(self, validated_data):
+        video_url_data = validated_data.pop('video_url', None)
+        post = Post.objects.create(**validated_data)
+        if video_url_data:
+            video_prompt = VideoPrompt.objects.create(**video_url_data)
+            post.video_url = video_prompt
+            post.save()
+        return post
+
+    def update(self, instance, validated_data):
+        video_url_data = validated_data.pop('video_url', None)
+        instance.title = validated_data.get('title', instance.title)
+        instance.content = validated_data.get('content', instance.content)
+        instance.image = validated_data.get('image', instance.image)
+        instance.save()
+
+        if video_url_data:
+            if instance.video_url:
+                for attr, value in video_url_data.items():
+                    setattr(instance.video_url, attr, value)
+                instance.video_url.save()
+            else:
+                video_prompt = VideoPrompt.objects.create(**video_url_data)
+                instance.video_url = video_prompt
+                instance.save()
+
+        return instance
+
 
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
